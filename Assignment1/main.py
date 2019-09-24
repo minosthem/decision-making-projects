d5d99defc1c40d3749a3afc80c609ff6516e14f2
minosthem <minosthemelis@gmail.com>
2019-09-24 16:17:11 +0200

    start part 5

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -1,4 +1,4 @@
-from questions import part1, part2And3
+from questions import part1, part2And3, part5
 import utils
 
 
@@ -7,3 +7,5 @@ if __name__ == '__main__':
     instances = part1.generate_problem_instances()
     # part 2 & 3
     part2And3.run_knapsack_for_problem_instance(instances[0], utils.capacity)
+    # part 4 & 5
+    part5.run_gurobipy()
