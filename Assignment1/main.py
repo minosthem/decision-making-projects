2c9cd61bfaa3ad3eb2b51a2a0d408200250667f2
minosthem <minosthemelis@gmail.com>
2019-09-28 20:23:06 +0200

    todo main

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     # part 1
     instances, weight_tuples = part1.generate_problem_instances()
     # part 2 & 3
-    part2And3.run_knapsack_for_problem_instance(instances[0], utils.capacity)
+    # TODO uncomment the below
+    #part2And3.run_knapsack_for_problem_instance(instances[0], utils.capacity)
     # part 4 & 5
     part5.run_gurobipy(instances)
