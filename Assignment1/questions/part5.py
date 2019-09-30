a4aa83c50574492e45f43d77ba04c66b437f6ebc
minosthem <minosthemelis@gmail.com>
2019-09-30 12:08:07 +0300

    add comments and documentation in part5.py

--- Assignment1/questions/part5.py
+++ Assignment1/questions/part5.py
@@ -6,20 +6,20 @@ import utils
 
 
 def run_gurobipy(problem_instances):
+    print("Running gurobi for each problem instance")
     for i, problem_instance in enumerate(problem_instances):
+        print("Creating model for problem instance {}".format(i))
         model = gb.Model('MILP')
-        item_indx = list(range(10))
-        senarios = get_all_combinations()
-        new_senarios, revenues, new_decision_combs, probabilities = get_model_data(senarios, problem_instance.items)
+        item_indx = list(range(len(problem_instance.items)))
+        scenarios, revenues, decision_combs, probabilities = get_model_data(problem_instance.items)
         obj = gb.LinExpr()
-        for j, senario in enumerate(new_senarios):
+        for j, scenario in enumerate(scenarios):
             senario_obj = gb.LinExpr()
             sizes = model.addVars(item_indx, vtype=gb.GRB.CONTINUOUS, name="sizes{}".format(j), lb=0)
-
-            total_size = total_selected_size(senario, new_decision_combs[j])
+            total_size = total_selected_size(scenario, decision_combs[j])
             tu = total_size - utils.capacity
             for k in item_indx:
-                senario_obj += sizes[k] * (senario[k] * revenues[k] * new_decision_combs[j][k])
+                senario_obj += sizes[k] * (scenario[k] * revenues[k] * decision_combs[j][k])
             if total_size > utils.capacity:
                 senario_obj -= utils.penalty * (total_size - utils.capacity)
             senario_obj *= probabilities[j]
@@ -30,22 +30,25 @@ def run_gurobipy(problem_instances):
         model.update()
         model.optimize()
 
+        print("Showing variables and objective function values for problem instance {}".format(i))
         for v in model.getVars():
             print('%s %g' % (v.varName, v.X))
         obj = model.getObjective()
         print('Profit: %g' % -obj.getValue())
 
 
-def get_all_combinations():
-    return list(product(['dl', 'dh'], repeat=10))
-
-
-def get_decision_combinations():
-    return list(product([0, 1], repeat=10))
-
-
-def get_model_data(senarios, items):
-    new_senarios = []
+def get_model_data(items):
+    """
+    Based on the combinations for dl and dh, all sizes are created
+    in the list new_scenarios. The function generates the combinations
+    for sizes, decision variables, the possibility of each scenario
+    and a list of the revenue of each item of the problem instance
+    :param items: the problem instance items
+    :return: four lists with the scenarios (i.e. sizes), the revenues, the
+    decision variables and the probabilities
+    """
+    new_scenarios = []
+    senarios = get_all_combinations()
     decision_combs = get_decision_combinations()
     new_decision_combs = []
     revenues = get_item_revenues_vector(items)
@@ -60,17 +63,48 @@ def get_model_data(senarios, items):
             decision_vars[j] = dec[j]
             prob = item.pi if senario[j] == 'dh' else (1 - item.pi)
             senario_prob *= prob
-        new_senarios.append(new_senario)
+        new_scenarios.append(new_senario)
         new_decision_combs.append(decision_vars)
         probabilities.append(senario_prob)
-    return new_senarios, revenues, new_decision_combs, probabilities
+    return new_scenarios, revenues, new_decision_combs, probabilities
+
+
+def get_all_combinations():
+    """
+    Uses product function from itertools to create all combinations of dl and dh
+    for the ten items of each problem instance
+    :return: the combinations
+    """
+    return list(product(['dl', 'dh'], repeat=10))
+
+
+def get_decision_combinations():
+    """
+    Uses product function from itertools to generate all
+    possible combinations of values 0 and 1 for the 10 items
+    of each problem instance
+    :return: the list of the combinations
+    """
+    return list(product([0, 1], repeat=10))
 
 
 def get_item_revenues_vector(items):
+    """
+    Creates a list with the revenue of each item
+    :param items: the problem instance items
+    :return: list with the respective revenues
+    """
     return [item.r for item in items]
 
 
 def total_selected_size(sizes, decision_vars):
+    """
+    Calculates the total selected size based on the
+    decision variables and the item sizes
+    :param sizes: the sizes of the items
+    :param decision_vars: list with binary values (0,1)
+    :return: the total size of the items with decision variable 1
+    """
     total_size = 0
     for i, decision_var in enumerate(decision_vars):
         if decision_var == 1.0:
