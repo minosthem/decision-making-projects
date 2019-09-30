30a6a58db510f1ecd6cf0b2d6a19169c84a9b41f
minosthem <minosthemelis@gmail.com>
2019-09-30 13:06:49 +0300

    small update in main

--- Assignment1/main.py
+++ Assignment1/main.py
@@ -2,11 +2,17 @@ from questions import part1, part2And3, part5
 import utils
 
 
-if __name__ == '__main__':
+def main():
     # part 1
     instances = part1.generate_problem_instances()
     # part 2 & 3
-    # TODO uncomment the below
-    #part2And3.run_knapsack_for_problem_instance(instances[0], utils.capacity)
+    part2And3.run_knapsack_for_problem_instance(instances[0], utils.capacity)
     # part 4 & 5
+    part5.run_gurobi(instances)
+
+
+if __name__ == '__main__':
+    # TODO remove all line and uncomment main()
+    # main()
+    instances = part1.generate_problem_instances()
     part5.run_gurobi(instances)
