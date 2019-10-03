5b63966a5a50f8615727d74876b889358f9461a5
minosthem <minosthemelis@gmail.com>
2019-10-03 13:01:50 +0200

    fix gurobi

--- Assignment1/questions/part5.py
+++ Assignment1/questions/part5.py
@@ -10,91 +10,78 @@ def run_gurobi(problem_instances, properties, output_folder):
 
     print("Running gurobi for each problem instance")
     for i, problem_instance in enumerate(problem_instances):
-        item_indx = list(range(len(problem_instance.items)))
-        scenarios, revenues, decision_combs, probabilities = get_model_data(problem_instance.items)
+        create_model_for_problem_instance(problem_instance=problem_instance, i=i, capacity=capacity, penalty=penalty,
+                                          output_folder=output_folder)
 
-        print("Creating model for problem instance {}".format(i))
-        model = gb.Model('MILP')
-        obj = gb.LinExpr()
 
-        for j, scenario in enumerate(scenarios):
-            for z, decision_var in enumerate(decision_combs):
-                # create model variable for scenario j and decision var vector z
-                sizes = model.addVars(item_indx, vtype=gb.GRB.CONTINUOUS, name="sizes{}{}".format(j, z), lb=0)
-                # calculate the total selected size based on the decision variables
-                total_size_selected = total_selected_size(scenario, decision_var)
-                # calculate the penalty based on the capacity and selected weights
-                final_penalty = penalty * (total_size_selected - capacity)
-                # calculate the objective function of the current scenario and decision var vector
-                obj += probabilities[j] * (sum((scenario[k] * revenues[k] * decision_var[k]) * sizes[k]
-                                               for k in item_indx) - final_penalty if final_penalty > 0 else sum(
-                    (scenario[k] * revenues[k] * decision_var[k]) * sizes[k] for k in item_indx))
-                rhs = total_size_selected - capacity
-                # TODO unbounded model??
-                model.addConstr(lhs=gb.quicksum(sizes[k] for k in item_indx if decision_var[k] == 1),
-                                sense=gb.GRB.LESS_EQUAL, rhs=capacity, name="noPenalty{}{}".format(j, z))
-                model.addConstr(lhs=gb.quicksum(sizes[k] for k in item_indx) - capacity,
-                                sense=gb.GRB.GREATER_EQUAL, rhs=rhs, name="scenario{}{}".format(j, z))
-                model.addConstr(lhs=gb.quicksum(sizes[k] for k in item_indx) - capacity,
-                                sense=gb.GRB.GREATER_EQUAL, rhs=0, name="scenarioPositive{}{}".format(j, z))
-        # set the objective function to the model
-        print("Setting total objective function to model {}".format(i))
-        model.setObjective(obj, gb.GRB.MAXIMIZE)
-        print("Updating model {}".format(i))
-        # update the model
-        model.update()
-        model.feasRelaxS(relaxobjtype=0, minrelax=True, vrelax=False, crelax=True)
-        print("Optimizing model {}".format(i))
-        # optimize the model
-        model.optimize()
-        check_model_status(model, i, output_folder)
+def create_model_for_problem_instance(problem_instance, i, capacity, penalty, output_folder):
+    item_indx = list(range(len(problem_instance.items)))
+    scenarios, revenues, probabilities = get_model_data(problem_instance.items)
+
+    print("Creating model for problem instance {}".format(i))
+    model = gb.Model('MILP')
+    obj = gb.LinExpr()
+
+    print("Executing scenarios")
+    for j, scenario in enumerate(scenarios):
+        execute_scenario(model=model, obj=obj, scenario=scenario, j=j, item_indx=item_indx, capacity=capacity,
+                         penalty=penalty, probabilities=probabilities, revenues=revenues)
+    # set the objective function to the model
+    print("Setting total objective function to model {}".format(i))
+    model.setObjective(obj, gb.GRB.MAXIMIZE)
+    print("Updating model {}".format(i))
+    # update the model
+    model.update()
+    print("Optimizing model {}".format(i))
+    # optimize the model
+    model.optimize()
+    print("Getting model results")
+    check_model_status(model, i, output_folder)
+
+
+def execute_scenario(model, obj, scenario, j, item_indx, capacity, penalty, probabilities, revenues):
+    total_revenues = get_total_revenues(scenario, revenues)
+    # create model variables for scenario j
+    decision_vars = model.addVars(item_indx, vtype=gb.GRB.BINARY, name="decision_var{}".format(j), lb=0)
+    # calculate the objective function of the current scenario and decision var vector
+    obj += probabilities[j] * (sum(total_revenues[k] * decision_vars[k] for k in item_indx) - (
+            penalty * (sum(scenario[k] * decision_vars[k] for k in item_indx) - capacity)))
+
+    # add constraints
+    model.addConstr(lhs=(sum(scenario[k] * decision_vars[k] for k in item_indx) - capacity),
+                    sense=gb.GRB.GREATER_EQUAL, rhs=0, name="constraint{}".format(j))
 
 
 def get_model_data(items):
     """
     Based on the combinations for dl and dh, all sizes are created
     in the list new_scenarios. The function generates the combinations
-    for sizes, decision variables, the possibility of each scenario
-    and a list of the revenue of each item of the problem instance
+    for sizes, the possibility of each scenario and a list of the revenue
+    of each item of the problem instance
     :param items: the problem instance items
-    :return: four lists with the scenarios (i.e. sizes), the revenues, the
-    decision variables and the probabilities
+    :return: four lists with the size combinations, the revenues and the probabilities
     """
     new_scenarios = []
     scenarios = list(product(['dl', 'dh'], repeat=len(items)))
-    decision_combs = list(product([0, 1], repeat=len(items)))
-    new_decision_combs = []
     revenues = [item.r for item in items]
     probabilities = []
     for i, scenario in enumerate(scenarios):
-        new_scenario = {}
-        dec = decision_combs[i]
-        decision_vars = {}
+        new_scenario = []
         scenario_prob = 1
         for j, item in enumerate(items):
-            new_scenario[j] = item.dl if scenario[j] == 'dl' else item.dh
-            decision_vars[j] = dec[j]
+            new_scenario.append(item.dl if scenario[j] == 'dl' else item.dh)
             prob = item.pi if scenario[j] == 'dh' else (1 - item.pi)
             scenario_prob *= prob
         new_scenarios.append(new_scenario)
-        new_decision_combs.append(decision_vars)
         probabilities.append(scenario_prob)
-    return new_scenarios, revenues, new_decision_combs, probabilities
+    return new_scenarios, revenues, probabilities
 
 
-def total_selected_size(sizes, decision_vars):
-    """
-    Calculates the total selected size based on the
-    decision variables and the item sizes
-    :param sizes: the sizes of the items
-    :param decision_vars: list with binary values (0,1)
-    :return: the total size of the items with decision variable 1
-    """
-    total_size = 0
-    for i, decision_var in enumerate(decision_vars):
-        if decision_var == 1.0 or decision_var == 1:
-            total_size += sizes[i]
-    return total_size
+def get_total_revenues(sizes, revenues):
+    total_revenues = []
+    for i, size in enumerate(sizes):
+        total_revenues.append(size * revenues[i])
+    return total_revenues
 
 
 def check_model_status(model, problem_instance, output_folder):
