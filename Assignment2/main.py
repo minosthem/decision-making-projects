082291e8cea93b9cba746f8032ddbe6a11049f45
minosthem <minosthemelis@gmail.com>
2019-11-02 17:28:57 +0200

    assignment2

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,30 +1,73 @@
 from os.path import join, exists
-from models.models import Server
+from models.models import Server, Customer
 import yaml
 import os
 
 properties_folder = join(os.getcwd(), "properties")
 example_properties_file = join(properties_folder, "example_properties.yaml")
 properties_file = join(properties_folder, "properties.yaml")
-
+properties_file = "/home/sissy/Documents/minos/decision-making-projects/Assignment2/properties/properties.yaml"
 
 def load_properties():
     """
     Load yaml file containint program's properties
     :return: the properties dictionary and the output folder path
     """
-    file = properties_file if exists(properties_file) else example_properties_file
-    with open(file, 'r') as f:
+    ffile = properties_file if exists(properties_file) else example_properties_file
+    with open(ffile, 'r') as f:
         properties = yaml.safe_load(f)
     return properties
 
 
 def main():
     properties = load_properties()
-    servers = []
-    for i in range(properties["servers_num"]):
-        server = Server(properties["server_capacity"])
+    servers, needy_customers, content_customers = [], [], []
+
+    for _ in range(properties["servers_num"]):
+        # add servers
+        server = Server()
         servers.append(server)
+        # add customers
+        needy_customers.append(Customer(prob_stay=properties["prob_stay"], exp_params=properties["exp_params"]))
+
+    # simulate
+    while True:
+        # assign customers to free servers
+        for s in servers:
+            if not s.is_occupied():
+                # get a needy customer
+                cust = needy_customers.pop(0)
+                # assign them to the server
+                cust.become_served(s)
+
+        # see if waiting / serving / ...  times are over
+        # check if servign time is over
+        for s in servers:
+            if s.is_occupied():
+                # get customer they're serving
+                cust = s.customer
+                # check if he's served enough
+                if cust.is_done_being_served():
+                    # release server
+                    s.release()
+                    # complete customer service state
+                    cust.complete_being_served()
+                    # decide if he'll become content or leave
+                    left = cust.decide_to_leave_or_not()
+                    if left:
+                        results = cust.get_waiting_times()
+                    else:
+                        cust.become_content()
+                        content_customers.append(cust)
+
+        # check if customers are done being content
+        for cust in content_customers:
+            if cust.is_done_being_content():
+                cust.complete_being_content() 
+                # remove from the list
+                content_customers.remove(cust)
+                needy_customers.append(cust)
+    
 
 
 if __name__ == '__main__':
--- Assignment2/models/models.py
+++ Assignment2/models/models.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import numpy as np
+import random
 
 
 class Customer:
@@ -11,15 +13,57 @@ class Customer:
     state = ""
     priority = ""
 
-    def __init__(self, arrival_time=datetime.now(), service_time=datetime.now(), end_patience_time=datetime.now(),
-                 state="Needy", priority="Low", prob=0.5):
+    def __init__(self, arrival_time=datetime.now(), state="Needy", priority=None, prob_stay=0.5, exp_params=(0.5,0.6)):
         self.arrival_time = arrival_time
-        self.service_time = service_time
-        self.system_arrival_time = arrival_time
-        self.end_patience_time = end_patience_time
-        self.state = state
+        self.started_being_served_time = None
+
+        self.prob_stay = prob_stay
         self.priority = priority
-        self.prob_to_stay = prob
+        self.mu, self.delta = exp_params
+
+        # durations:
+        # times waiting to be served
+        self.wait_times_to_be_served = []
+        # times during which you are served
+        self.served_times = []
+        # times during which you are content
+        self.content_times = []
+
+    def decide_to_leave(self):
+        if random.random() < self.prob_stay:
+            # stay
+            return False
+        else:
+            # leave
+            return True
+
+    def become_served(self, server):
+        self.server = server
+        server.customer = self
+        server.occupied = True
+        # time waited to be served
+        self.wait_times_to_be_served.append(datetime.now() - self.arrival_time)
+        # compute the time the customer needs
+        self.service_time_needed = np.random.exponential(self.mu)
+        self.started_being_served_time = datetime.now()
+
+    def is_done_being_served(self):
+        return (datetime.now() - self.started_being_served_time) >= self.service_time_needed
+
+    def is_done_being_content(self):
+        return (datetime.now() - self.started_being_content) >= self.content_time_needed
+
+    def completed_being_served(self):
+        self.served_times.append(self.service_time_needed)
+
+    def become_content(self):
+        # calculate content duration
+        self.content_time_needed = np.random.exponential(self.delta)
+        self.started_being_content = datetime.now()
+
+    def completed_being_content(self):
+        self.content_times.append(self.content_time_needed)
+        self.arrival_time = datetime.now()
 
     def move_to(self, location, time, new_service_time):
         self.location = location
@@ -31,32 +75,16 @@ class Customer:
         self.service_time = -1
 
 
-class Event:
-    arrival = 0
-    departure = 1
-    abandonment = 2
-    type = ""
-    time = 0
-    customer = Customer()
-
-    def __init__(self, typ, time, cust):
-        self.type = typ
-        self.time = time
-        self.customer = cust
-
-    def __lt__(self, other):
-        return self.time < other.time
-
-    def __str__(self):
-        s = ('Arrival', 'Departure', 'Abandonment')
-        return s[self.type] + " of customer {}".format(self.customer) + "at time {}".format(self.time)
-
-
 class Server:
-    capacity = 0
+    occupied = False
+    customer = None
 
-    def __init__(self, capacity):
-        self.capacity = capacity
+    def __init__(self):
+        self.occupied = False
+    def is_occupied(self):
+        return self.occupied
+    def release(self):
+        self.occupied, self.customer = False, None
 
 
 class Queue:
